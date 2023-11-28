05752ef9abd1588f80e967b56af6c60b59e42733
Fernanda Dias <fernandadias9@hotmail.com>
2023-11-28 16:54:27 -0300

    Update WinRest.java
    mudança no método usando a classe CarteiraDigitalVO

--- Cassino/cassino/src/main/java/controller/WinRest.java
+++ Cassino/cassino/src/main/java/controller/WinRest.java
@@ -5,9 +5,12 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import model.bo.BetBO;
+import model.dao.BetDAO;
 import model.dao.WinDAO;
+import model.vo.BetVO;
+import model.vo.CarteiraDigitalVO;
 import model.vo.WinVO;
-
 
 @Path("/win")
 public class WinRest {
@@ -15,10 +18,15 @@ public class WinRest {
 	@POST	
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String registerBet(WinVO winVO) {
+	public CarteiraDigitalVO registerBet(WinVO winVO) {
 		WinDAO winDAO = new WinDAO();
-		winDAO.registerWinDAO(winVO);
+		CarteiraDigitalVO carteira = new CarteiraDigitalVO();
+		winDAO.registerWinDAO(winVO);		
 		winDAO.updatePlayerBalanceDAO(winVO);		
-		return "player: " + winVO.getIdPlayer() + "\nbalance: " + winDAO.checkPlayerBalanceDAO(winVO) + "\ntxn: " + winVO.getIdTransaction();
+		double finalBalance = winDAO.checkPlayerBalanceDAO(winVO);
+		carteira.setPlayer(winVO.getIdPlayer());
+		carteira.setBalance(finalBalance);
+		carteira.setTxn(winVO.getIdTransaction());
+		return carteira;
 	}
-}
+}	
