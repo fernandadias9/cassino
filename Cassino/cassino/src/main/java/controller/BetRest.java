52d3fa308eb8a7650a25b8c628d7dd8589c50b51
Fernanda Dias <fernandadias9@hotmail.com>
2023-11-28 16:53:20 -0300

    Update BetRest.java
    mudança no método usando a classe CarteiraDigitalVO

--- Cassino/cassino/src/main/java/controller/BetRest.java
+++ Cassino/cassino/src/main/java/controller/BetRest.java
@@ -8,18 +8,24 @@ import jakarta.ws.rs.core.MediaType;
 import model.bo.BetBO;
 import model.dao.BetDAO;
 import model.vo.BetVO;
+import model.vo.CarteiraDigitalVO;
 
 @Path("/bet")
 public class BetRest {
-
+	
 	@POST	
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String registerBet(BetVO betVO) {
+	public CarteiraDigitalVO registerBet(BetVO betVO) {
 		BetBO bet = new BetBO();
 		BetDAO betDAO = new BetDAO();
+		CarteiraDigitalVO carteira = new CarteiraDigitalVO();
 		bet.registerBetBO(betVO);
-		betDAO.updatePlayerBalanceDAO(betVO);		
-		return "player: " + betVO.getIdPlayer() + "\nbalance: " + betDAO.checkPlayerBalanceDAO(betVO) + "\ntxn: " + betVO.getIdTransaction();
+		betDAO.updatePlayerBalanceDAO(betVO);
+		double finalBalance = betDAO.checkPlayerBalanceDAO(betVO);
+		carteira.setPlayer(betVO.getIdPlayer());
+		carteira.setBalance(finalBalance);
+		carteira.setTxn(betVO.getIdTransaction());
+		return carteira;		
 	}
 }
